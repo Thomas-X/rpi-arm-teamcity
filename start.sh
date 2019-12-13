c2858836ce7f74ecb7fa09958c44bca8ff59cd90
Thomas Zwarts <thomaszwarts@gmail.com>
2019-12-13 20:16:43 +0100

    .

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+mkdir dist
+wget -o teamcity.tar.gz https://download-cf.jetbrains.com/teamcity/TeamCity-2019.1.5.tar.gz --show-progress
+tar zxf teamcity.tar.gz -C dist/
+rm -rf teamcity.tar.gz
+docker rm teamcitypi --force
+docker image build -t teamcitypi .
+docker container run --name teamcitypi -it teamcitypi:latest
